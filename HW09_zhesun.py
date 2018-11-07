e8f467ef74b96852c07bbec14cbcd99beef7fd17
RobertZSun <44449668+RobertZSun@users.noreply.github.com>
2018-11-07 14:11:27 -0500

    add major file and process the require&elective courses
    read one more file: majors.txt, and we process the majors and reprint the student prettytable, with adding remaining required&elective courses

--- HW09_zhesun.py
+++ HW09_zhesun.py
@@ -1,16 +1,16 @@
 #! C:\Users\Administrator\AppData\Local\Programs\Python\Python37
 # -*- coding: utf-8 -*-
 """
-Created  on Wednesday Oct 31 14:09:00 2018
+Created  on Wednesday Nev 7 14:09:00 2018
 
 @author: zhe sun
 
 This file including there parts, Part 1: class Student
                                  Part 2: class Instructor
                                  Part 3: class University
-                                 Part 4：make prettytable of Part 1&2
+                                 Part 4: class Major
+                                 Part 5：make prettytable of Part 1&2
 """
-
 import os
 import unittest
 from HW08_zhesun import file_reader
@@ -20,28 +20,48 @@ from collections import defaultdict
 
 class Homework09Test(unittest.TestCase):
 
-    def test_Student_test(self):
+    def test_PrintTables_test(self):
 
-        stevens = University("C:\\Users\Administrator\Desktop\810self",
+        stevens = University("C:\\Users\Administrator\Desktop\810",
                              ptables=True)
-        sdetail = {tuple(student.returnlist())
-                   for student in stevens._students.values()}
+        sdetail = [tuple(student.returnlist())
+                   for student in stevens._students.values()]
         idetail = {tuple(row) for instructor in stevens._instructors.values()
                    for row in instructor.returnlist()}
-        sresult = {('10103', 'Baldwin, C', 'SFEN', ('CS 501', 'SSW 564',
-                                                    'SSW 567', 'SSW 687')),
-                   ('10115', 'Wyatt, X', 'SFEN', (
-                       'CS 545', 'SSW 564', 'SSW 567', 'SSW 687')),
-                   ('10172', 'Forbes, I', 'SFEN', ('SSW 555', 'SSW 567')),
-                   ('10175', 'Erickson, D', 'SFEN', (
-                       'SSW 564', 'SSW 567', 'SSW 687')),
-                   ('10183', 'Chapman, O', 'SFEN', ('SSW 689',)),
-                   ('11399', 'Cordova, I', 'SYEN', ('SSW 540',)),
-                   ('11461', 'Wright, U', 'SYEN', (
-                       'SYS 611', 'SYS 750', 'SYS 800')),
-                   ('11658', 'Kelly, P', 'SYEN', ('SSW 540',)),
-                   ('11714', 'Morton, A', 'SYEN', ('SYS 611', 'SYS 645')),
-                   ('11788', 'Fuller, E', 'SYEN', ('SSW 540',))}
+        mdetail = [major.returnlist() for major in stevens._majors.values()]
+
+        sresult = [('10103', 'Baldwin, C', 'SFEN', {'SSW 564', 'CS 501',
+                                                    'SSW 687', 'SSW 567'},
+                                           {'SSW 540', 'SSW 555'}, None),
+                   ('10115', 'Wyatt, X', 'SFEN', {'SSW 564', 'SSW 687',
+                                                  'CS 545', 'SSW 567'},
+                    {'SSW 540', 'SSW 555'}, None),
+                   ('10172', 'Forbes, I', 'SFEN', {'SSW 555', 'SSW 567'},
+                    {'SSW 564', 'SSW 540'}, {'CS 545', 'CS 501', 'CS 513'}),
+                   ('10175', 'Erickson, D', 'SFEN', {'SSW 564', 'SSW 687',
+                                                     'SSW 567'},
+                    {'SSW 540', 'SSW 555'}, {'CS 545', 'CS 501', 'CS 513'}),
+                   ('10183', 'Chapman, O', 'SFEN', {'SSW 689'},
+                    {'SSW 564', 'SSW 540', 'SSW 555', 'SSW 567'},
+                    {'CS 545', 'CS 501', 'CS 513'}),
+                   ('11399', 'Cordova, I', 'SYEN', {'SSW 540'},
+                    {'SYS 800', 'SYS 671', 'SYS 612'}, None),
+                   ('11461', 'Wright, U', 'SYEN',
+                    {'SYS 800', 'SYS 750', 'SYS 611'}, {'SYS 671', 'SYS 612'},
+                    {'SSW 540', 'SSW 565', 'SSW 810'}),
+                   ('11658', 'Kelly, P', 'SYEN', None,
+                    {'SYS 800', 'SYS 671', 'SYS 612'},
+                    {'SSW 540', 'SSW 565', 'SSW 810'}),
+                   ('11714', 'Morton, A', 'SYEN', {'SYS 645', 'SYS 611'},
+                    {'SYS 800', 'SYS 671', 'SYS 612'},
+                    {'SSW 540', 'SSW 565', 'SSW 810'}),
+                   ('11788', 'Fuller, E', 'SYEN', {'SSW 540'},
+                    {'SYS 800', 'SYS 671', 'SYS 612'}, None)]
+
+        mresult = [['SFEN', {'SSW 567', 'SSW 564', 'SSW 540', 'SSW 555'},
+                    {'CS 501', 'CS 545', 'CS 513'}],
+                   ['SYEN', {'SYS 800', 'SYS 671', 'SYS 612'},
+                    {'SSW 565', 'SSW 810', 'SSW 540'}]]
 
         iresult = {('98765', 'Einstein, A', 'SFEN', 'SSW 567', 4),
                    ('98765', 'Einstein, A', 'SFEN', 'SSW 540', 3),
@@ -61,14 +81,18 @@ class Homework09Test(unittest.TestCase):
 
 
 class Student:
-    pt_labels = ["CWID", "Name", "Majo", "Completed Courses"]
+    pt_labels = ["CWID", "Name", "Major", "Completed Courses",
+                 "Remaining Required", "Remaining Electives"]
 
-    def __init__(self, cwid, name, major):
+    def __init__(self, cwid, name, major, class_major):
         self._cwid = cwid
         self._name = name
         self._major = major
+        self._class_major = class_major
 
         self._courses = defaultdict(str)  # _courses[course] = grade
+        self._remaining_req = set()
+        self._remaining_ele = set()
 
     def add_course(self, course, grade=""):
         """ student earns grade in course """
@@ -80,10 +104,62 @@ class Student:
                 f"{sorted(self._courses.keys())}")
 
     def returnlist(self):
-        return [self._cwid,
-                self._name,
-                self._major,
-                tuple(sorted(self._courses.keys()))]
+        """return a list of values to populate the prettytable
+           for this student """
+        completed_courses, remain_required, remain_electives \
+            = self._class_major.grade_check(self._courses)
+        return [self._cwid, self._name, self._major,
+                completed_courses, remain_required, remain_electives]
+
+
+class Major:
+    pt_labels = ["Dept", "Required Courses", "Electives Courses"]
+
+    def __init__(self, dept, passed_grades=None):
+        self._dept = dept
+        # _electives = {"SSW 810", "SSW 690", "SSW 564"}
+        self._required = set()
+        # _electives = {"SSW 810", "SSW 690", "SSW 564"}
+        self._electives = set()
+        if passed_grades is None:
+            self._pass_grades = {'A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C'}
+        else:
+            self._pass_grades = set(passed_grades)
+
+    def add_course(self, flag, course):
+        """ addd courses from the file majors.txt"""
+        if flag == 'R':
+            self._required.add(course)  # add course to required courses list
+        elif flag == 'E':
+            self._electives.add(course)  # add course to elective courses list
+        else:
+            raise ValueError(
+                f"This {flag} {course} is not our expected course")
+
+    def grade_check(self, courses):
+        """ to calculate completed_courses, remain_required, remain_electives
+            from a dict[course] = grade from a class Student instance
+        """
+        completed_courses = {
+            course for course, grade in courses.items(
+            ) if grade in self._pass_grades}
+        remain_required = self._required - completed_courses
+        if self._electives.intersection(completed_courses):
+            remain_electives = None
+        else:
+            remain_electives = self._electives
+        if completed_courses == set():
+            completed_courses = None
+        return completed_courses, remain_required, remain_electives
+
+    def __str__(self):
+        return (f"Dept: {self._dept} Required Courses: {self._required}"
+                f" Electives Courses: {self._electives}")
+
+    def returnlist(self):
+        return [self._dept,
+                self._required,
+                self._electives]
 
 
 class Instructor:
@@ -114,20 +190,38 @@ class Instructor:
 class University:
 
     def __init__(self, path_dir, ptables=True):
-        self._students = dict()
-        self._instructors = dict()
+        self._path_dir = path_dir  # directory of all the files to be read
+        self._students = dict()  # key: cwid value: class Student
+        self._instructors = dict()  # key: cwid value: class Instructor
         self._grades = list()
+        self._majors = dict()  # key: name of the major value: class Major
 
+        self._read_majors(os.path.join(path_dir, "majors.txt"))
         self._read_students(os.path.join(path_dir, "students.txt"))
         self._read_instructors(os.path.join(path_dir, "instructors.txt"))
         self._read_grades(os.path.join(path_dir, "grades.txt"))
 
         if ptables:
+            print("\nMajor Summary")
+            self.major_prettytable()
+
             print("\nStudent Summary")
             self.student_prettytable()
 
             print("\nInstructor Summary")
             self.instructor_prettytable()
+
+    def _read_majors(self, path):
+        """ read the majors files and store
+            the data in self._majors """
+        try:
+            for major, flag, course in file_reader(path, 3,
+                                                   sep='\t', header=False):
+                if major not in self._majors:
+                    self._majors[major] = Major(major)
+                self._majors[major].add_course(flag, course)
+        except ValueError as error:
+            print(error)
 
     def _read_instructors(self, path):
         """ read the instructors files and
@@ -145,7 +239,7 @@ class University:
 
     def _read_students(self, path):
         """ read the students files and store
-        the data in self._students """
+            the data in self._students """
         try:
             for cwid, name, major in file_reader(path, 3,
                                                  sep='\t', header=False):
@@ -153,7 +247,8 @@ class University:
                     print(
                         f"Warning: cwid {cwid} already read from the file")
                 else:
-                    self._students[cwid] = Student(cwid, name, major)
+                    self._students[cwid] = Student(
+                        cwid, name, major, self._majors[major])
         except ValueError as error:
             print(error)
 
@@ -179,10 +274,19 @@ class University:
                 print(f"Warning: instructor cwid {instructor_cwid}"
                       " is not in the instructor file")
 
+    def major_prettytable(self):
+        pt = PrettyTable(field_names=Major.pt_labels)
+        for major in self._majors.values():
+            pt.add_row(major.returnlist())
+        print(pt)
+
     def student_prettytable(self):
         pt = PrettyTable(field_names=Student.pt_labels)
         for student in self._students.values():
             pt.add_row(student.returnlist())
+        # sdetail = [tuple(student.returnlist())
+        #            for student in self._students.values()]
+        # print(f"sdetail: {sdetail}")
         print(pt)
 
     def instructor_prettytable(self):
@@ -195,8 +299,8 @@ class University:
 
 def main():
     unittest.main(exit=False, verbosity=2)
-    stevens = University("C:\\Users\Administrator\Desktop\810self"
-                         ptables=True)
+    university_path = "C:/Users/Administrator/Desktop/810"
+    stevens = University(university_path, ptables=True)
 
 
 if __name__ == '__main__':
